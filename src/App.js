33c55f50ca49eb32014b5d5e70e863b99c1a088a
Amaan Jaweed <ajaweed@ur.rochester.edu>
2024-02-06 11:34:05 -0500

    Input form added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
 import Header from "./Components/Header.jsx";
+import Input from "./Components/Input.jsx";
 function App() {
   return (
     <div className="App">
       <Header />
+      <Input />
     </div>
   );
 }
